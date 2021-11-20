830a706464e19c959e77b7155bc6b33d60ddf711
Ankur Dhaduk <ankurdhaduk1988@gmail.com>
2021-11-21 00:08:46 +0530

    ok with docker

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const controller = require("../controller/controller");
 
 let routes = (app) => {
+  router.post("/", (req , res) => {
+    res.send('sai...');
+  });
   router.post("/upload", controller.upload);
   //router.get("/files", controller.getListFiles);
 
